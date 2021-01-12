61a75e1a31b45f239274a74d7d5d395a64da477d
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-12 03:16:35 -0300

    Updates

---
+++ precache-manifest.6b8160444cab52258f551be97c80cf8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4297bca241d38997798db8da59baaaf",
+    "url": "/Tarjeta/index.html"
+  },
+  {
+    "revision": "7fc86894e09dcae77006",
+    "url": "/Tarjeta/static/css/main.1b0e2528.chunk.css"
+  },
+  {
+    "revision": "28524b92e1bc115666e4",
+    "url": "/Tarjeta/static/js/2.c87d7cd8.chunk.js"
+  },
+  {
+    "revision": "6ee87c57e2a7b3f0e0b10d639394c036",
+    "url": "/Tarjeta/static/js/2.c87d7cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc86894e09dcae77006",
+    "url": "/Tarjeta/static/js/main.74320bb9.chunk.js"
+  },
+  {
+    "revision": "92d85308af7163dc4e64",
+    "url": "/Tarjeta/static/js/runtime-main.a8dea73a.js"
+  },
+  {
+    "revision": "7cf0ee8b7a9e8287267792040650dc7a",
+    "url": "/Tarjeta/static/media/chip.7cf0ee8b.png"
+  }
+]);
